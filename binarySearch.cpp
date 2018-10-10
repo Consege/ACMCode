f83704dd5ae9650d58b30738c241d68ed34ea855
KuangJun <fantacykj@gmail.com>
2018-10-10 21:12:23 +0800

    Update binarySearch.cpp

--- binarySearch.cpp
+++ binarySearch.cpp
@@ -1,4 +1,4 @@
-  //两边都取的写法
+  //查找最后一个满足条件的元素
   int lft = 0 , rgt = n ;
         while(lft!=rgt){
             int mid = (lft+rgt+1)>>1 ;
@@ -10,3 +10,13 @@
             }
 
         }
+
+//查找第一个满足条件的元素
+
+int lft=0,rgt=n+1,mid;
+while(lft<rgt){
+    mid=lft+rgt>>1;
+    if(can(mid)) rgt=mid;
+    else lft=mid+1;
+}
+return lft;
