22d6346a15a8eb0454b80dc22cf2183edec719aa
crisjk <kjmd2d@sina.cn>
2019-05-13 11:16:41 +0800

    updata binary search

--- binarySearch.cpp
+++ binarySearch.cpp
@@ -1,7 +1,11 @@
+  //总结:如何避免死循环: 如果left = mid+1 那么while(left<right) mid = (left+right)/2  
+  //否则:如果left= mid 那么while(left+1<right) mid = (left+right+1)/2
+  //以后再也不难写2分了
+  
   //查找最后一个满足条件的元素
   int lft = 0 , rgt = n ;
         while(lft!=rgt){
-            int mid = (lft+rgt+1)>>1 ;
+            int mid = (lft+rgt+1)>>1 ; 
             if(can(mid)){
                 lft = mid ;
             }
